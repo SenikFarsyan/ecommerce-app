cd5732152b78fffd565fa76389d885ec88f4c2a5
Senik <farsyan.senik@gmail.com>
2019-08-15 22:33:55 +0400

    updated shop data, update cart utils to group carts in cart reducer

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
+//actions
+import { addItem } from '../../redux/cart/cart.actions';
+
+//components
+import CustomButton from '../custom-button/custom-button.component';
 
 //styles
 import './collection-item.styles.scss';
 
-const CollectionItem = ({ id, name, price, imageUrl }) => {
+const CollectionItem = ({ item, addItem }) => {
+  const { name, price, imageUrl } = item;
+  const handleClick = () => addItem(item);
   return (
     <div className='collection-item'>
       <div className='image' style={{ backgroundImage: `url(${imageUrl})` }} />
@@ -11,8 +20,20 @@ const CollectionItem = ({ id, name, price, imageUrl }) => {
         <span className='name'>{name}</span>
         <span className='price'>{price}</span>
       </div>
+      <CustomButton inverted onClick={handleClick}>
+        Add to cart
+      </CustomButton>
     </div>
   );
 };
 
-export default CollectionItem;
+const mapDispatchToProps = dispatch => {
+  return {
+    addItem: item => dispatch(addItem(item))
+  };
+};
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(CollectionItem);
--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -3,10 +3,17 @@ import React from 'react';
 //styles
 import './custom-button.styles.scss';
 
-const CostomButton = ({ children, isGoogleSignIn, ...otherProps }) => {
+const CostomButton = ({
+  children,
+  isGoogleSignIn,
+  inverted,
+  ...otherProps
+}) => {
   return (
     <button
-      className={`${isGoogleSignIn ? 'google-sign-in' : ''}  custom-button`}
+      className={`${inverted ? 'inverted' : ''} ${
+        isGoogleSignIn ? 'google-sign-in' : ''
+      }  custom-button`}
       {...otherProps}
     >
       {children}
