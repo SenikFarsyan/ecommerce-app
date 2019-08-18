067759d1da0a81bfbf373b026a7e0cec5710601b
Senik <farsyan.senik@gmail.com>
2019-08-18 13:02:59 +0400

    update stripe integration

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -10,6 +10,7 @@ import {
 
 //components
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+import StrpeCheckoutButton from '../../components/stripe-button/stripe-button.component';
 
 //styles
 import './checkout.styles.scss';
@@ -40,6 +41,12 @@ const Checkout = ({ cartItems, total }) => {
       <div className='total'>
         <span>TOTAL: ${total}</span>
       </div>
+      <div className='test-warning'>
+        *Please use the following test credit card for payments*
+        <br />
+        4242 4242 4242 4242 - Exp: 01/20 - CVV: 123
+      </div>
+      <StrpeCheckoutButton price={total} />
     </div>
   );
 };
